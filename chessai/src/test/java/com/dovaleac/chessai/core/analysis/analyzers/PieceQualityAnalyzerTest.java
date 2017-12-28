46708ce16802e46a8303dfb55b464a6984e7e3b3
Pablo González <dovaleac@gmail.com>
2017-12-28 12:49:31 +0100

    Number of moves works

--- chessai/src/test/java/com/dovaleac/chessai/core/analysis/analyzers/PieceQualityAnalyzerTest.java
+++ chessai/src/test/java/com/dovaleac/chessai/core/analysis/analyzers/PieceQualityAnalyzerTest.java
@@ -3,6 +3,7 @@ package com.dovaleac.chessai.core.analysis.analyzers;
 import com.dovaleac.chessai.core.Color;
 import com.dovaleac.chessai.core.Position;
 import com.dovaleac.chessai.core.PositionFactory;
+import com.dovaleac.chessai.core.analysis.positional_facts.AbstractNumberAndPiecePositionalFact;
 import com.dovaleac.chessai.core.analysis.positional_facts.AbstractOnePiecePositionalFact;
 import com.dovaleac.chessai.core.analysis.positional_facts.PositionalFact;
 import com.dovaleac.chessai.core.pieces.Piece;
@@ -39,8 +40,35 @@ class PieceQualityAnalyzerTest {
     assertEquals(3, facts
         .stream()
         .filter(it -> it instanceof AbstractOnePiecePositionalFact.KnightIsInOutpost
-        || it instanceof AbstractOnePiecePositionalFact.BishopIsInOutpost)
-    .count());
+            || it instanceof AbstractOnePiecePositionalFact.BishopIsInOutpost)
+        .count());
+  }
+
+  @Test
+  void analyzeNumberOfMoves() {
+    Position position = PositionFactory.fromString("Qa1,a2,b2,Nb1,d2,Kf1,Rh1,g2#Bh4,f6,Rd8,Rd7,Kf8##w#");
+    List<PositionalFact> facts = analyzer.analyze(position).collect(Collectors.toList());
+
+    facts.forEach(System.out::println);
+    assertTrue(facts.contains(new AbstractNumberAndPiecePositionalFact.NumberOfSquaresKnightCanReach(
+        2, Piece.fromNotation("Nb1", Color.WHITE))));
+    assertTrue(facts.contains(new AbstractNumberAndPiecePositionalFact.NumberOfSquaresQueenCanReach(
+        0, Piece.fromNotation("Qa1", Color.WHITE))));
+    assertTrue(facts.contains(new AbstractNumberAndPiecePositionalFact.NumberOfSquaresRookCanReach(
+        4, Piece.fromNotation("Rh1", Color.WHITE))));
+    assertTrue(facts.contains(new AbstractNumberAndPiecePositionalFact.NumberOfSquaresBishopCanReach(
+        4, Piece.fromNotation("Bh4", Color.BLACK))));
+    assertTrue(facts.contains(new AbstractNumberAndPiecePositionalFact.NumberOfSquaresRookCanReach(
+        4, Piece.fromNotation("Rd8", Color.BLACK))));
+    assertTrue(facts.contains(new AbstractNumberAndPiecePositionalFact.NumberOfSquaresRookCanReach(
+        12, Piece.fromNotation("Rd7", Color.BLACK))));
+    assertEquals(6, facts
+        .stream()
+        .filter(it -> it instanceof AbstractNumberAndPiecePositionalFact.NumberOfSquaresKnightCanReach
+            || it instanceof AbstractNumberAndPiecePositionalFact.NumberOfSquaresBishopCanReach
+            || it instanceof AbstractNumberAndPiecePositionalFact.NumberOfSquaresRookCanReach
+            || it instanceof AbstractNumberAndPiecePositionalFact.NumberOfSquaresQueenCanReach)
+        .count());
   }
 
 }
